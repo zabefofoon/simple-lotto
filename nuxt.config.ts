2fe0b37de7b11e1fbac181d48e81bf05eb8f88f0
정상우 <zabefofoon@gmail.com>
2023-12-22 13:48:55 +0900

    github action 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
+const path = require('path')
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  ssr: true,
   css: ['~/assets/style/main.scss'],
   postcss: {
     plugins: {
@@ -8,4 +11,24 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+  app: {
+    baseURL: '/simple-lotto/',
+    buildAssetsDir: 'assets',
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/simple-lotto/favicon.ico' },
+      ]
+  },
+  },
+  routeRules: {
+    '/': { prerender: true },
+  },
+  nitro: {
+    output: {
+      publicDir: path.join(__dirname, '/docs'),
+    },
+  },
+  experimental: {
+    payloadExtraction: false
+  }
 })
